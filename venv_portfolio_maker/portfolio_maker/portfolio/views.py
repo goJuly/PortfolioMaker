8d0d3540b6f3b64717ff043a01aaeab77ea639a4
goJuly <shinmori.g@gmail.com>
2020-08-29 01:26:00 +0900

    ひな形だけ。

---
+++ venv_portfolio_maker/portfolio_maker/portfolio/views.py
@@ -0,0 +1,11 @@
+import logging
+
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+logger = logging.getLogger(__name__)
+
+
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
